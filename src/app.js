64a3c1db3dc57cd45353ad3a5d1728ebb5dff18a
Mridulbirla13 <mridulbirla13@gmail.com>
2024-06-25 23:40:29 +0530

    started integrating jwt

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const signupRoute = require("./routes/signup");
 const cors = require('cors');
 const app = express();
+const createAdminAccount = require("./scripts/admin");
 const PORT = process.env.PORT || 5000;
 
 
@@ -10,6 +11,8 @@ app.use(cors({
 }));
 
 app.use(express.json());
+
+createAdminAccount();
 
 app.use("/user", signupRoute);
 
---
+++ src/configuration/jwtConfig.js
@@ -0,0 +1,34 @@
+const crypto = require("crypto");
+
+const secretKey = crypto.randomBytes(32).toString('hex')
+
+module.exports = {
+    secretKey: secretKey
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
